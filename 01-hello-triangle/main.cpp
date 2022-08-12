e8ba7b08d2d99f4d630f6527151e478269742871
José Expósito <jose.exposito89@gmail.com>
2022-08-12 18:12:33 +0200

    Hello triangle: Creating an instance

--- 01-hello-triangle/main.cpp
+++ 01-hello-triangle/main.cpp
@@ -21,6 +21,7 @@ public:
 
 private:
     GLFWwindow *window;
+    VkInstance instance;
 
     void initWindow() {
         glfwInit();
@@ -32,7 +33,33 @@ private:
     }
 
     void initVulkan() {
+        createInstance();
+    }
 
+    void createInstance() {
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
     }
 
     void mainLoop() {
