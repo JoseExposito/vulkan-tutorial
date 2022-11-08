c09c53da5bdecae52e4cbf150eb71e8a212227fe
José Expósito <jose.exposito89@gmail.com>
2022-11-08 09:24:14 +0200

    Hello triangle: Drawing: Recording the command buffer

--- 01-hello-triangle/main.cpp
+++ 01-hello-triangle/main.cpp
@@ -835,6 +835,9 @@ private:
 
         uint32_t imageIndex;
         vkAcquireNextImageKHR(device, swapChain, UINT64_MAX, imageAvailableSemaphore, VK_NULL_HANDLE, &imageIndex);
+
+        vkResetCommandBuffer(commandBuffer, 0);
+        recordCommandBuffer(commandBuffer, imageIndex);
     }
 
     void cleanup() {
